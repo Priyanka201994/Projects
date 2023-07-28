614fc428c454b1c60dee11004851ef78983e795b
Priyanka201994 <140794454+Priyanka201994@users.noreply.github.com>
2023-07-28 20:17:09 +0530

    Update AP.py

--- AP.py
+++ AP.py
@@ -2,7 +2,7 @@ import requests
 api_key="8b66a510658c972be58a9ee2f59bce3b"
 cityName=input("Enter cityName:")
 weatherData=requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=8b66a510658c972be58a9ee2f59bce3b&units=metric".format(cityName))
-print(weatherData.status_code)
+#print(weatherData.status_code)
 if(weatherData.status_code==404):
     print("Enter A valid city Name");
 else:
