7af88566972a760762f32c47303d8208b09dd489
Priyanka201994 <140794454+Priyanka201994@users.noreply.github.com>
2023-07-28 15:03:13 +0530

    invalid city name

--- AP.py
+++ AP.py
@@ -1,7 +1,12 @@
 import requests
 api_key="8b66a510658c972be58a9ee2f59bce3b"
-cityName=input("Enter cityName")
+cityName=input("Enter cityName:")
 weatherData=requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=8b66a510658c972be58a9ee2f59bce3b&units=metric".format(cityName))
-temp=weatherData.json()
-temperature=temp["main"]["temp"]
-print("The temperature of city:",cityName,"is",temperature)
+print(weatherData.status_code)
+if(weatherData.status_code==404):
+    print("Enter A valid city Name");
+else:
+    temp=weatherData.json()
+    temperature=temp["main"]["temp"]
+    print("The temperature of city:",cityName,"is",temperature,"degree celsius")
+
